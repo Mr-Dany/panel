0658c65ecb761b465358016cec55eaacd6004348
robcuero <50973521+robcuero@users.noreply.github.com>
2021-02-09 20:19:29 -0500

    UNO-MUCHOS

---
+++ Dominio/tipo_usuarios.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Dominio
+{
+    public class tipo_usuarios
+    {
+        [Key]
+        public int IdTipoUsuarios { get; set; }
+        public string tipo { get; set; }
+        public bool eliminado { get; set; }
+
+        public ICollection<usuario> usuariolista { get; set; }
+        
+        
+    }
+}
